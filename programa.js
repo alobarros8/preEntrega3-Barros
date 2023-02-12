3df4e97a206de34c2c53ba743593351cfd8fc04b
alobarros8 <116090399+alobarros8@users.noreply.github.com>
2023-02-12 16:22:29 -0300

    Create programa.js

---
+++ programa.js
@@ -0,0 +1,66 @@
+let burgers [{
+nombre: Gran Chamaco
+cantidad:1
+precio:$1500
+ingredintes:
+},{
+nombre: Cheese burger
+cantidad:1
+precio:$1300
+ingredintes:
+},{
+nombre: Cheese bacon
+cantidad:1
+precio:$1400
+ingredintes:
+},{
+nombre:La chamaca
+cantidad:1
+precio:$1600
+ingredintes:
+},{
+nombre:Riquifort
+cantidad:1
+precio:$1600
+ingredintes:
+},{
+nombre:Yankee
+cantidad:1
+precio:$1550
+ingredintes:
+},{
+nombre:Winsconsin
+cantidad:1
+precio:$1500
+ingredintes:
+},{
+nombre:Voraz
+cantidad:1
+precio:$1500
+ingredintes:
+},{
+nombre:Oklahoma
+cantidad:1
+precio:$1550
+ingredintes:
+},{
+nombre:Crispyoli
+cantidad:1
+precio:$1650
+ingredintes:
+},{
+nombre:Classic
+cantidad:1
+precio:$1500
+ingredintes:
+},{
+nombre:Eggbacon
+cantidad:1
+precio:$1550
+ingredintes:
+},{
+nombre:Chamapork
+cantidad:1
+precio:$1650
+ingredintes:
+}]
